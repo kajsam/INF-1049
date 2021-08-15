226b6dd1a2e2a881d4a162198ca7d05b49a4a716
Kajsa Møllersen <kajsa.mollersen@uit.no>
2021-08-15 19:42:01 +0200

    Done Thursday

--- Week01/regning.py
+++ Week01/regning.py
@@ -13,3 +13,14 @@ print(0.1*(3/3)) # uten parantes blir dette ikke 0.1
 a = 144
 
 print(math.sqrt(a))
+
+# variabler
+
+x = 2 / 5
+print("x = :", x)
+x = x + 10
+print ("x = : ", x)
+
+a = 11
+b = 5
+print("Rest:", a % b)
