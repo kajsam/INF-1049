c7f0136fa68bcc1a3610e9b2fc206c079a72d665
Kajsa Møllersen <kajsa.mollersen@uit.no>
2021-08-15 19:27:57 +0200

    New folder stucture

---
+++ Week01/matematikk.py
@@ -0,0 +1,8 @@
+
+print("Summen er:", 2+5)
+print("2/5:", 2/5)
+print("11//5:", 11//5)
+
+print("2^5:", 2^5) # bitwise XOR operator (don't know what that is)
+
+print("2**5:", 2**5)
---
+++ Week01/regning.py
@@ -0,0 +1,15 @@
+""" 
+Med tre anforselstegn kommenterer man ut hele driten imellom.
+"""
+
+import math # importerer en pakke (skriver dette overst)
+
+# Kommenterer ut resten av linja:
+
+print("Hello, world!")
+
+print(0.1*(3/3)) # uten parantes blir dette ikke 0.1
+
+a = 144
+
+print(math.sqrt(a))
